5bbbd7dda282ea13a9cfb4ad0bbc149e84ba440e
谢雨轩 <xieyuxuan@xieyuxuandeMacBook-Pro.local>
2018-07-30 08:24:27 +0800

    webpack4.0搭建前端项目

---
+++ app/main.js
@@ -0,0 +1,26 @@
+//import * as React from 'react';
+//import * as ReactDOM from 'react-dom';
+//
+//import style from './styles/common';
+//
+//class Main extends React.Component{
+//
+//    constructor(props){
+//        super(props);
+//    }
+//
+//    //render(){
+//    //    return(
+//    //        <h1>你真棒</h1>
+//    //    )
+//    //}
+//}
+//
+//ReactDOM.render(
+//    <Main />,
+//    document.getElementById("lbt")
+//);
+
+import * as styles from './styles/index.css';
+import * as demo  from './styles/demo.less';
+console.log("娃哈哈");
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const glob = require('glob');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+
+module.exports = {
+    entry: {
+        main: './app/main.js'
+    },
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: '[name].js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(css|less$)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","less-loader","postcss-loader"]
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: 'assets/[name]-[hash].[ext]',
+                            limit: 8192,
+                            outputPath: ''
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './app/index.html'
+        }),
+        new ExtractTextPlugin("common.css"),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'app/*.html'))
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        host: 'localhost',
+        port: 9000
+    }
+};
